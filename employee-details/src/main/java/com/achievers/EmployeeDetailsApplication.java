935a3a7c61f0b57e96d2dd2fafd1aa941a1fda38
suneely19 <tech.suneel19@gmail.com>
2022-10-15 09:35:14 +0530

    Project structure created

---
+++ employee-details/src/main/java/com/achievers/EmployeeDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.achievers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDetailsApplication.class, args);
+	}
+
+}
